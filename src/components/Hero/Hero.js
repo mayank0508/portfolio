00a70cab548766315688e2ead5d05b024b2ce712
mayank0508 <thakurmayank0508@gmail.com>
2021-06-16 12:51:32 +0530

    header, hero done 💯😭

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = props => (
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+        Hola ! Welcome to <br />
+        My Personal Space
+      </SectionTitle>
+      <SectionText> The purpose of this shit is to fuck off </SectionText>
+      <Button
+        onClick={() =>
+          (window.location = 'https://www.google.com/search?q=fuckoff')
+        }
+      >
+        {' '}
+        Go Fuck OFF
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img
+} from './ProjectsStyles';
+import {
+  Section,
+  SectionDivider,
+  SectionTitle
+} from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {[
+        {
+          title: 'Project 1',
+          description: 'Majje wale project'
+        },
+        {
+          title: 'Project 1',
+          description: 'Majje wale project'
+        },
+      ].map(project => (
+        <div>
+          {project.title}
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
